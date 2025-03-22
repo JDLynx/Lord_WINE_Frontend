d82bae375bd86dcad571046571680cc503fbb3d4
JDLynx <jdavid.lynx@gmail.com>
2025-03-22 15:08:30 -0500

    Dependencias Tailwind

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Header'
+
+function Header()
+{
+    return(
+    <div>
+        <h1 id="LordImg">
+            <img src="/img/Logo LORD WINE.png" alt="Logo de LORD WINE" className="logo-img" />
+        </h1>
+    </div>
+    )
+}
+
+export default Header
