347329a0e7bbd0308f342118c1c0815a10b38f25
JDLynx <jdavid.lynx@gmail.com>
2025-03-26 17:58:59 -0500

    Proyecto actualizado

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,11 @@
 import { useState } from 'react' 
-
-
 import './App.css'
-import Home from './vistas/Home'
-import Login from './vistas/Login'
-import CarritoCompras from './vistas/CarritoCompras'
-import PuntosVenta from './vistas/PuntosVenta'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <PuntosVenta />
     </>
   )
 }
