9c994d1757d19bddc29f9d8eb535feb9f4bdbdd2
JuanSalazar77 <salazarpazjuanjose@gmail.com>
2025-03-28 10:23:29 -0500

    Ultima Actualización rama JuanJose

--- src/vistas/Album.jsx
+++ src/vistas/Album.jsx
@@ -1,25 +1,58 @@
 import React from 'react'
+import { useState } from "react";
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import BarraProductos from "../components/BarraProductos";
 import "./Album.css";
 
-function Album()
-{
-    return(
+export default function Album() {
+    const images = [
+        "../../img/LORD WINE Slide - 1.jpeg",
+        "../../img/LORD WINE Slide - 2.jpg",
+        "../../img/LORD WINE Slide - 3.jpeg",
+      ];
+    
+      const [currentIndex, setCurrentIndex] = useState(0);
+    
+      const prevSlide = () => {
+        setCurrentIndex((prevIndex) =>
+          prevIndex === 0 ? images.length - 1 : prevIndex - 1
+        );
+      };
+    
+      const nextSlide = () => {
+        setCurrentIndex((prevIndex) =>
+          prevIndex === images.length - 1 ? 0 : prevIndex + 1
+        );
+      };
+    
+      return (
         <>
-        <div className="page-container">
-
-        <Header />
-        <BarraProductos />
-
-        <main>Desarrollar el contenido</main>
-
-        <Footer />
-
-        </div>
+            <div className="page-container bg-gray-900 text-white min-h-screen flex flex-col">
+            <Header />
+            <BarraProductos />
+        
+            <h2 className="text-center text-white text-3xl my-4">Álbum</h2>
+        
+            <main className="flex flex-col items-center justify-center flex-grow">
+                <div className="relative w-96 h-96 overflow-hidden">
+                {images.map((src, index) => (
+                    <img
+                    key={index}
+                    src={src}
+                    alt={`Imagen ${index + 1}`}
+                    className={`absolute top-0 left-0 w-full h-full object-cover transition-opacity duration-500 ${
+                        index === currentIndex ? "opacity-100" : "opacity-0"
+                    }`}
+                    />
+                ))}
+                    <button onClick={prevSlide} className="carousel-button left-4">❮</button>
+                    <button onClick={nextSlide} className="carousel-button right-4">❯ </button>
+                </div>
+            </main>
+        
+            <Footer />
+            </div>
         </>
-    )
-}
-
-export default Album
+        );
+    }
