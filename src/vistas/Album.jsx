033fecdaed455d7e6a6a7a236586d90fd1d19f4c
JuanSalazar77 <salazarpazjuanjose@gmail.com>
2025-03-31 10:16:22 -0500

    Ultima actualizacion de rama JuanJose

--- src/vistas/Album.jsx
+++ src/vistas/Album.jsx
@@ -31,24 +31,48 @@ export default function Album() {
             <div className="page-container bg-gray-900 text-white min-h-screen flex flex-col">
             <Header />
             <BarraProductos />
-        
-            <h2 className="text-center text-white text-3xl my-4">Álbum</h2>
-        
-            <main className="flex flex-col items-center justify-center flex-grow">
-                <div className="relative w-96 h-96 overflow-hidden">
-                {images.map((src, index) => (
-                    <img
-                    key={index}
-                    src={src}
-                    alt={`Imagen ${index + 1}`}
-                    className={`absolute top-0 left-0 w-full h-full object-cover transition-opacity duration-500 ${
-                        index === currentIndex ? "opacity-100" : "opacity-0"
-                    }`}
-                    />
-                ))}
-                    <button onClick={prevSlide} className="carousel-button left-4">❮</button>
-                    <button onClick={nextSlide} className="carousel-button right-4">❯ </button>
+
+            <main className="flex justify-around py-4 px-4">
+              
+              <div className="flex items-center justify-center p-6 shadow-lg">
+                <div className= "p-8 bg-black rounded-[10px]" style={{ backgroundColor: "rgba(0, 0, 0, 0.8)" }}>
+                  <h2 className="text-center text-white text-3xl my-4">Fotos</h2>
+                  <div className="relative w-96 h-96 m-8">
+                  {images.map((src, index) => (
+                      <img
+                      key={index}
+                      src={src}
+                      alt={`Imagen ${index + 1}`}
+                      className={`absolute top-0 left-0 w-full h-full object-cover transition-opacity duration-500 ${
+                          index === currentIndex ? "opacity-100" : "opacity-0"
+                      }`}
+                      />
+                  ))}
+                      <button onClick={prevSlide} className="carousel-button left-4">❮</button>
+                      <button onClick={nextSlide} className="carousel-button right-4">❯ </button>
+                  </div>
                 </div>
+              </div>
+
+              <div className="flex items-center justify-center p-6 shadow-lg">
+                <div className= "p-8 bg-black rounded-[10px]" style={{ backgroundColor: "rgba(0, 0, 0, 0.8)" }}>
+                  <h2 className="text-center text-white text-3xl my-4">Videos</h2>
+                  <div className="relative w-96 h-96 m-8">
+                  {images.map((src, index) => (
+                      <img
+                      key={index}
+                      src={src}
+                      alt={`Imagen ${index + 1}`}
+                      className={`absolute top-0 left-0 w-full h-full object-cover transition-opacity duration-500 ${
+                          index === currentIndex ? "opacity-100" : "opacity-0"
+                      }`}
+                      />
+                  ))}
+                      <button onClick={prevSlide} className="carousel-button left-4">❮</button>
+                      <button onClick={nextSlide} className="carousel-button right-4">❯ </button>
+                  </div>
+                </div>
+              </div>
             </main>
         
             <Footer />
