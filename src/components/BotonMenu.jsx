9f7c7262e29eb433e63be5ccb3471335a146bed5
JDLynx <jdavid.lynx@gmail.com>
2025-06-28 14:16:46 -0500

    Ajuste íconos y menús

--- src/components/BotonMenu.jsx
+++ src/components/BotonMenu.jsx
@@ -1,29 +1,42 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import { Link } from "react-router-dom";
 import "./BotonMenu.css";
 import { Bars3Icon } from "@heroicons/react/24/solid";
 
-function BotonMenu()
-{
-    const[isOpen, setIsOpen]=useState(false);
+function BotonMenu() {
+    const [isOpen, setIsOpen] = useState(false);
+    const menuRef = useRef(null);
 
-    return(
-        <div className="menu-container">
+    useEffect(() => {
 
-            <button className="btn-redondo" onClick={() => setIsOpen(!isOpen)}>
-                <Bars3Icon className="icono-menu" />
-            </button>
+        function handleClickOutside(event) {
+        if (menuRef.current && !menuRef.current.contains(event.target)) {
+            setIsOpen(false);
+        }
+        }
 
-            {isOpen && (
-                <ul className="dropdown-menu">
-                    <li><Link className="dropdown-item" to="/">Inicio</Link></li>
-                    <li><Link className="dropdown-item" to="/album">Álbum</Link></li>
-                    <li><Link className="dropdown-item" to="/contacto">Contacto</Link></li>
-                    <li><Link className="dropdown-item" to="/servicios-empresariales">Servicios empresariales</Link></li>
-                    <li><Link className="dropdown-item" to="/sobre-nosotros">Sobre nosotros</Link></li>
-                </ul>
-            )}
+        document.addEventListener("mousedown", handleClickOutside);
+        
+        return () => {
+        document.removeEventListener("mousedown", handleClickOutside);
+        };
+    }, []);
 
+    return (
+        <div className="menu-container" ref={menuRef}>
+        <button className="btn-redondo" onClick={() => setIsOpen(!isOpen)}>
+            <Bars3Icon className="icono-menu" />
+        </button>
+
+        {isOpen && (
+            <ul className="dropdown-menu">
+            <li><Link className="dropdown-item" to="/">Inicio</Link></li>
+            <li><Link className="dropdown-item" to="/album">Álbum</Link></li>
+            <li><Link className="dropdown-item" to="/contacto">Contacto</Link></li>
+            <li><Link className="dropdown-item" to="/servicios-empresariales">Servicios empresariales</Link></li>
+            <li><Link className="dropdown-item" to="/sobre-nosotros">Sobre nosotros</Link></li>
+            </ul>
+        )}
         </div>
     );
 }
